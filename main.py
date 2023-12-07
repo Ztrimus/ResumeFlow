18ed1c57c2eb05ab8010b6080690147470a5c319
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2023-12-07 03:55:37 -0700

    ENH: Refactor code for ease of package usage

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ Copyright (c) 2023 Saurabh Zinjad. All rights reserved | GitHub: Ztrimus
 -----------------------------------------------------------------------
 '''
 
+import os
 import argparse
-from zlm.scripts.auto_apply_pipeline import run_autoapply_pipeline
+from zlm import AutoApplyModel
 
-def create_resume(url, master_data):
-    run_autoapply_pipeline(url, master_data)
+def create_resume_cv(url, master_data, openai_key, downloads_dir):
+    job_llm = AutoApplyModel(openai_key, downloads_dir)
+    job_llm.resume_cv_pipeline(url, master_data)
 
 if __name__ == "__main__":
     # Create an argument parser
@@ -22,8 +24,16 @@ if __name__ == "__main__":
 
     parser.add_argument('-u', "--url", help='URL of the job posting')
     parser.add_argument('-m', "--master_data", default="master_data/user_profile.json", help='Path of user\'s master data file.')
+    parser.add_argument('-k', "--openai_api_key", default="os", help='Open AI API Keys')
+    parser.add_argument('-d', "--downloads_dir", help='Give detailed path of folder')
 
     # Parse the arguments
     args = parser.parse_args()
 
-    run_autoapply_pipeline(args.url, args.master_data)
+    args.url = "https://boards.greenhouse.io/thebrattlegroup/jobs/4325755005"
+    args.master_data = "/home/saurabh/AAA/Convergent/Projects/job-llm/master_data/user_profile.json"
+
+    if args.openai_api_key == 'os':
+        args.openai_api_key = os.environ.get("OPENAI_API_KEY")
+    
+    create_resume_cv(args.url, args.master_data, args.openai_api_key, args.downloads_dir)
