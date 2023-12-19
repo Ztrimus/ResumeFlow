77f38359489d58674c5b558cf0f807d8a3656017
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2023-12-18 18:10:52 -0700

    ENH: Add Provider options for different LLM Usage

--- main.py
+++ main.py
@@ -12,20 +12,21 @@ import argparse
 from zlm import AutoApplyModel
 
 
-def create_resume_cv(url, master_data, openai_key, downloads_dir):
+def create_resume_cv(url, master_data, api_key, provider, downloads_dir):
     """
     Creates a resume or CV using the Job-LLM model.
 
     Args:
         url (str): The URL of the job posting or description.
         master_data (dict): The master data containing information about the candidate.
-        openai_key (str): The API key for OpenAI.
+        api_key (str): The API key for OpenAI.
+        provider (str): The LLM provider to use. Currently, only "together, openai" is supported.
         downloads_dir (str): The directory where the generated resume or CV will be saved.
 
     Returns:
         None
     """
-    job_llm = AutoApplyModel(openai_key, downloads_dir)
+    job_llm = AutoApplyModel(api_key, provider, downloads_dir)
     job_llm.resume_cv_pipeline(url, master_data)
 
 
@@ -37,12 +38,13 @@ if __name__ == "__main__":
 
     parser.add_argument("-u", "--url", help="URL of the job posting")
     parser.add_argument("-m", "--master_data", help="Path of user's master data file.")
-    parser.add_argument("-k", "--openai_api_key", default="os", help="Open AI API Keys")
+    parser.add_argument("-k", "--api_key", default="os", help="LLM Provider API Keys")
     parser.add_argument("-d", "--downloads_dir", help="Give detailed path of folder")
+    parser.add_argument("-p", "--provider", default="openai", help="LLM provider name. support for openai, together, g4f")
 
     # Parse the arguments
     args = parser.parse_args()
 
     create_resume_cv(
-        args.url, args.master_data, args.openai_api_key, args.downloads_dir
+        args.url, args.master_data, args.api_key, args.provider, args.downloads_dir
     )
