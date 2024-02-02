baa836aabc16ed5749b5e9e139eb9387df734c89
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-02-02 01:25:26 -0700

    FIX #16: Trial and Error

--- web_app.py
+++ web_app.py
@@ -100,7 +100,6 @@ if get_resume_button or get_cover_letter_button:
     
         # Extract user data
         user_data = resume_llm.user_data_extraction(file_path, is_st_print=True)
-        st.info(user_data)
 
         shutil.rmtree(os.path.dirname(file_path))
 
@@ -109,8 +108,6 @@ if get_resume_button or get_cover_letter_button:
             job_details = resume_llm.job_details_extraction(url=url, is_st_print=True)
         elif text != "":
             job_details = resume_llm.job_details_extraction(job_site_content=text, is_st_print=True)
-
-        st.info(job_details)
 
         # Build Resume
         if get_resume_button:
--- zlm/__init__.py
+++ zlm/__init__.py
@@ -315,8 +315,7 @@ class AutoApplyModel:
 
                 llm = self.get_llm_instance(system_prompt)
                 response = llm.get_response(query, expecting_longer_output=True, need_json_output=True)
-                st.write(resume_details)
-                st.markdown("---")
+                st.toast(resume_details)
                 time.sleep(2)
                 if section in response:
                     resume_details[section] = response[section]
@@ -324,20 +323,18 @@ class AutoApplyModel:
             if 'keywords' in job_details:
                 resume_details['keywords'] = job_details['keywords']
             
-            st.write(f"self.downloads_dir: {self.downloads_dir}")
-            st.markdown("---")
+            st.toast(f"self.downloads_dir: {self.downloads_dir}")
             resume_path = job_doc_name(job_details, self.downloads_dir, "resume")
             
-            st.write(f"resume_path: {resume_path}")
-            st.markdown("---")
+            st.toast(f"resume_path: {resume_path}")
 
             write_json(resume_path, resume_details)
 
             resume_path = resume_path.replace(".json", ".pdf")
 
             pdf_data, resume_latex = latex_to_pdf(resume_details, resume_path)
-            st.write(f"resume_path: {resume_path}")
-            st.write(f"resume_latex: {resume_latex}")
+            st.toast(f"resume_path: {resume_path}")
+            st.toast(f"resume_latex: {resume_latex}")
             print("Resume PDF generated at: ", resume_path)
             return resume_path, resume_details
         except Exception as e:
