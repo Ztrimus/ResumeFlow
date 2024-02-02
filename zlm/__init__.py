1cb718bb625cfbc9896e08ea85ad98ac8548b90a
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-02-02 01:33:34 -0700

    Add job_details logging statement

--- zlm/__init__.py
+++ zlm/__init__.py
@@ -208,6 +208,7 @@ class AutoApplyModel:
 
             llm = self.get_llm_instance(system_prompt)
             job_details = llm.get_response(job_site_content, need_json_output=True)
+            st.write(f"job_details: {job_details}")
             if url is not None and url.strip() != "":
                 job_details["url"] = url
             jd_path = job_doc_name(job_details, self.downloads_dir, "jd")
